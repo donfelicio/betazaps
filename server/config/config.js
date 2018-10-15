e3350256f8f41b6c61cd81d8b5b4d2aded9c223f
Felix Lepoutre <felix@mrflow.co>
2018-10-15 17:12:46 +0200

    change to config to include json code

--- server/config/config.js
+++ server/config/config.js
@@ -8,3 +8,21 @@ if (env === 'development' || env === 'test') {
 		process.env[key] = envConfig[key]
 	});
 };
+
+//config.json file in same folder
+// {
+// 	"test": {
+// 		"PORT": 3000,
+// 		"MONGODB_URI": "mongodb://localhost:27017/DB_NAME",
+// 		"JWT_SECTRET": "SECRET_HERE",
+// 		"MAIL_USER": "USER_HERE",
+// 		"MAIL_PASS": "PASS_HERE"
+// 	}, 
+// 	"development": {
+// 		"PORT": 3000,
+// 		"MONGODB_URI": "mongodb://localhost:27017/DB_NAME",
+// 		"JWT_SECTRET": "SECRET_HERE",
+// 		"MAIL_USER": "USER_HERE",
+// 		"MAIL_PASS": "PASS_HERE"
+// 	}
+// }
